876bb98146fa636de534c5078603af1a0ddb692f
Nick Lewis <nick@TechEd-0006.techeducators.co.uk>
2023-08-07 13:44:26 +0100

    updateClient

---
+++ client/src/Components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <div>
+      <h1>Spotifind</h1>
+      <nav>
+        <ul>
+          <li className="home">
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li className="about">
+            <Link to={"/about"}>About</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
