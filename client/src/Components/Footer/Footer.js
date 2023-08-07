238906938b7371eb2656f4dc475acad4c5a164b8
jow76 <joejardine76@gmail.com>
2023-08-07 16:09:20 +0100

    Actually pushing the form

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -2,5 +2,5 @@ import React from "react";
 import "./Footer.css";
 
 export default function Footer() {
-  return <footer>Elise, Shaylee, Joe and Nick</footer>;
+  return <footer>Elisa, Shaylee, Joe and Nick</footer>;
 }
