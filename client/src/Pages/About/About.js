f6d25033de66e11572ecd697403586bac6a09ac2
Shaghayegh Asadi <shaghayegh.asadi69@yahoo.com>
2023-08-08 17:39:34 +0100

    updatecss

--- client/src/Pages/About/About.js
+++ client/src/Pages/About/About.js
@@ -14,37 +14,28 @@ export default function About() {
         <link rel="canonical" href="/about" />
       </Helmet>
       <main>
-        <div className="about">
+        <div className="description">
           <h2>About</h2>
-          <p>
-            What is it? Spotifind is a single page web app that utilised the
-            Spotify API to allow a user to search through the full spotify
-            database of songs The problem: The spotify in-app search feature is
-            rather minimal and doesn't allow the user to apply filters to the
-            full extent that the search query is capable of, Spotifind will
-            allow users to more accurately search songs
+          <p className="about">
+            Spotifind is a single page web app that utilised the Spotify API to
+            allow a user to search through the full spotify database of songs
+            The problem: The spotify in-app search feature is rather minimal and
+            doesn't allow the user to apply filters to the full extent that the
+            search query is capable of, Spotifind will allow users to more
+            accurately search songs
+            <hr></hr>
+            &nbsp; The Team: Elisa, (github link) (linkedin link) Shaylee,
+            (github link) (linkedin link) Joe, (github link) (linkedin link)
+            Nick, (github link) (linkedin link) If you would like to look at the
+            code please feel free at the GitHub link below
+            <a
+              className="github"
+              href="https://github.com/ClownMaidLifestyle/Spotifind"
+            >
+              GitHub
+            </a>
           </p>
-        <h2>The Team:</h2>
-        
-          Here is the information about the creators of this website
-        
-
-        <h3 className="developer">Elisa, (github link) (linkedin link)</h3>
-        <h3 className="developer">Shaylee, (github link) (linkedin link)</h3>
-        <h3 className="developer">Joe, (github link) (linkedin link)</h3>
-        <h3 className="developer">Nick, (github link) (linkedin link)</h3>
-        <p className="moreInfo">
-          If you would like to look at the code please feel free at the GitHub
-          link below
-        </p>
-        <a
-          className="github"
-          href="https://github.com/ClownMaidLifestyle/Spotifind"
-        >
-          GitHub
-        </a>
-                </div>
-
+        </div>
       </main>
     </>
   );
